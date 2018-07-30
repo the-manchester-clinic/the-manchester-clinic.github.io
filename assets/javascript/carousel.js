79740c13af25735124f39a026e1df97b2c3906d2
Jonny Lyon <jonny.lyon@costcutter.com>
2018-07-30 20:15:09 +0100

    Initial version - homepage only so far

---
+++ assets/javascript/carousel.js
@@ -0,0 +1,22 @@
+function initCarousel() {
+	setTimeout(nextSlide, 5000);
+}
+
+function nextSlide() {
+	var slides = $(".image-slide");
+	var currentSlideIndex;
+	for (var i = 0; i < slides.length; i++) {
+		var slide = $(slides[i]);
+		if (slide.is(":visible")) {
+			currentSlideIndex = i;
+		}
+	}
+	var nextSlideIndex = currentSlideIndex + 1;
+	if (nextSlideIndex >= slides.length) {
+		nextSlideIndex = 0;
+	}
+	$(slides[nextSlideIndex]).fadeIn(1000);
+	$(slides[currentSlideIndex]).fadeOut(1000);
+	
+	setTimeout(nextSlide, 5000);
+}
