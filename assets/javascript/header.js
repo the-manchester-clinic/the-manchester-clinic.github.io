32c96e01e251b8c1107575f372e8e18a0202999c
Jonny Lyon <jonny.lyon@costcutter.com>
2018-07-30 21:05:46 +0100

    parameterising the header menu selection

--- assets/javascript/header.js
+++ assets/javascript/header.js
@@ -1,16 +1,29 @@
-const headerHTML = `
+		
+function initHeader(page) {
+	var homeCurrentClass = "";
+	var contactCurrentClass = "";
+	
+	switch (page) {
+		case "home":
+			homeCurrentClass = "current";
+			break;
+		case "contact":
+			contactCurrentClass = "current";
+			break;
+	}
+	
+	var headerHTML = `
 			<div class="header-bar">
 				<a href="index.html">
 					<img class="header-logo" src="assets/images/MOCS-Logo.png"></img>
 				</a>
 				<span class="header-menu">
-					<span class="header-menu-item current"><a href="index.html">Home</a></span>
-					<span class="header-menu-item"><a href="contact.html">Contact us</a></span>
+					<span class="header-menu-item ${homeCurrentClass}"><a href="index.html">Home</a></span>
+					<span class="header-menu-item ${contactCurrentClass}"><a href="contact.html">Contact us</a></span>
 				</span>
 				<div class="clear-float"></div>
 			</div>
 			<div class="horizontal-rule"></div>`;
-			
-function initHeader() {
+	
 	$("#header-bar-container").append(headerHTML);
 }
