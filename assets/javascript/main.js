a890952b30cc1e0c418dd766933903af8ce15410
Agni <agni@ilango.co.uk>
2019-02-10 17:41:26 +0000

    Change video host

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -281,15 +281,6 @@ function initLightbox() {
 }
 
 function initVideo(src) {
-  var video = `<div class="video">
-    <iframe
-      src=${src}
-      style="position:absolute;top:0;left:0;width:100%;height:100%;"
-      frameborder="0"
-      webkitallowfullscreen
-      mozallowfullscreen
-      allowfullscreen
-    />
-	</div>`;
-  $(video).insertAfter($(".video-container .headline:eq(0)"));
+  var video = `<script src="https://fast.wistia.com/embed/medias/e6fzrr78nz.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_e6fzrr78nz videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/e6fzrr78nz/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>`;
+  $(".video-container #video").append(video);
 }
